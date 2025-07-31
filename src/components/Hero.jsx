1ef535e0782abe59e8ab8981e961c8a256f3e324
NayeraGad <naiera.gad30@gmail.com>
2025-07-31 22:47:59 +0300

    navbar & hero section animation

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,6 +1,48 @@
-import React from "react";
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+import { SplitText } from "gsap/all";
 
 const Hero = () => {
+  useGSAP(() => {
+    const headerSplit = new SplitText("h1", {
+      type: "chars, words",
+    });
+
+    const paragraphSplit = new SplitText(".subtitle", {
+      type: "lines",
+    });
+
+    headerSplit.chars.forEach((char) => char.classList.add("header-gradient"));
+
+    gsap.from(headerSplit.chars, {
+      yPercent: 100,
+      duration: 1,
+      ease: "expo.out",
+      stagger: 0.06,
+    });
+
+    gsap.from(paragraphSplit.lines, {
+      opacity: 0,
+      yPercent: 100,
+      duration: 1.8,
+      ease: "expo.out",
+      stagger: 0.06,
+      delay: 1,
+    });
+
+    gsap
+      .timeline({
+        scrollTrigger: {
+          trigger: "#hero",
+          start: "top top",
+          end: "bottom top",
+          scrub: true,
+        },
+      })
+      .to(".hero-right-leaf", { y: 200 }, 0)
+      .to(".hero-left-leaf", { y: -200 }, 0);
+  }, []);
+
   return (
     <section id="hero">
       <h1>MOJITO</h1>
@@ -16,7 +58,7 @@ const Hero = () => {
           </div>
 
           <div className="hero-details">
-            <p>
+            <p className="subtitle">
               Every cocktail on our menu is a blend of premium ingredients,
               creative flair, and timeless recipes — designed to delight your
               senses.
