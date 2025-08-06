342f831aa8e49c7c1e398deaf2e1802aa222d286
NayeraGad <naiera.gad30@gmail.com>
2025-08-06 23:09:58 +0300

    Apply Art section animation

--- src/components/Art.jsx
+++ src/components/Art.jsx
@@ -1,15 +1,47 @@
 import React from "react";
 import { featureLists, goodLists } from "../constants";
 import { FaCheckCircle } from "react-icons/fa";
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+import { useMediaQuery } from "react-responsive";
 
 const Art = () => {
+  const isSmDevice = useMediaQuery({ maxWidth: 768 });
+
+  useGSAP(() => {
+    const maskTimeline = gsap.timeline({
+      scrollTrigger: {
+        trigger: "#art",
+        start: isSmDevice ? "top 20%" : "top top",
+        end: "bottom center",
+        scrub: 1.5,
+        pin: true,
+      },
+    });
+
+    maskTimeline
+      .to(".fade", {
+        opacity: 0,
+        ease: "power1.inOut",
+        stagger: 0.2,
+      })
+      .to(".cocktail-img img", {
+        scale: 1.3,
+        maskPosition: "center",
+        maskSize: "400%",
+        duration: 1,
+        ease: "power1.inOut",
+      })
+      .to(".masked-content", { opacity: 1, duration: 1, ease: "power1.inOut" });
+  });
+
   return (
     <section id="art">
       <div className="art-container">
-        <h2 className="z-10">The ART</h2>
+        <h2 className="fade">The ART</h2>
 
         <div className="art-content">
-          <ul className="features-list z-10">
+          <ul className="features-list fade">
             {goodLists.map((feature, i) => (
               <li key={`good ${i}`}>
                 <FaCheckCircle />
@@ -22,7 +54,7 @@ const Art = () => {
             <img src="/images/under-img.jpg" alt="cocktail image" />
           </div>
 
-          <ul className="features-list z-10">
+          <ul className="features-list fade">
             {featureLists.map((feature, i) => (
               <li key={`good ${i}`}>
                 <FaCheckCircle />
@@ -33,12 +65,12 @@ const Art = () => {
         </div>
 
         <div className="masked-container">
-          <h3>Sip-Worthy Perfection</h3>
+          <h3 className="fade">Sip-Worthy Perfection</h3>
 
-          <div id="masked-content">
+          <div className="masked-content">
             <h3>Made with Craft, Poured with Passion</h3>
             <p>
-              This isn’t just a drink. It’s a carefully crafted moment made just
+              This isn't just a drink. It's a carefully crafted moment made just
               for you.
             </p>
           </div>
