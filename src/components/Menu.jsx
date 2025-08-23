65c0a3c94870cbed672cc0936a7d0daf142fe6a2
NayeraGad <naiera.gad30@gmail.com>
2025-08-23 16:15:23 +0300

    Implement cocktail menu navigation and styling

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,6 +1,27 @@
+import { useState } from "react";
 import { allCocktails } from "../constants";
 
 const Menu = () => {
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const nextCocktail = () => {
+    if (currentIndex === allCocktails.length - 1) return setCurrentIndex(0);
+
+    setCurrentIndex(currentIndex + 1);
+  };
+
+  const prevCocktail = () => {
+    if (currentIndex === 0) return setCurrentIndex(allCocktails.length - 1);
+
+    setCurrentIndex(currentIndex - 1);
+  };
+
+  const goToSlide = (index) => {
+    setCurrentIndex(index);
+  };
+
+  let { image, name, title, description } = allCocktails[currentIndex];
+
   return (
     <section id="menu" aria-label="Cocktail menu">
       <img
@@ -17,37 +38,46 @@ const Menu = () => {
       />
 
       <nav>
-        {allCocktails.map((cocktail) => {
+        {allCocktails.map((cocktail, index) => {
           const { id, name } = cocktail;
+          const isActive = currentIndex === index;
 
-          return <button key={id}>{name}</button>;
+          return (
+            <button
+              key={id}
+              className={`${isActive ? "active" : null}`}
+              onClick={() => goToSlide(index)}
+            >
+              {name}
+            </button>
+          );
         })}
       </nav>
 
       <div className="menu-content">
         <div className="arrows">
-          <button aria-label="next arrow">
+          <button aria-label="previous arrow" onClick={prevCocktail}>
             <img src="/images/right-arrow.png" alt="" role="presentation" />
           </button>
 
-          <button aria-label="previous arrow">
+          <button aria-label="next arrow" onClick={nextCocktail}>
             <img src="/images/left-arrow.png" alt="" role="presentation" />
           </button>
         </div>
 
         <div className="cocktail">
-          <img src={allCocktails[0].image} alt={allCocktails[0].name} />
+          <img src={image} alt={name} />
         </div>
 
         <div className="recipe">
           <div className="titles">
             <p>Recipe for:</p>
-            <h2>{allCocktails[0].name}</h2>
+            <h2>{name}</h2>
           </div>
 
           <div className="details">
-            <h3>{allCocktails[0].title}</h3>
-            <p>{allCocktails[0].description}</p>
+            <h3>{title}</h3>
+            <p>{description}</p>
           </div>
         </div>
       </div>
